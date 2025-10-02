e7a092cfb546ed6573b4e58e83e70528d08705f8
Lexan <lexan713@gmail.com>
2025-10-02 12:42:28 -0400

    icon

--- IGTools/MainWindow.Designer.cs
+++ IGTools/MainWindow.Designer.cs
@@ -46,12 +46,14 @@
             button1.AutoSize = true;
             button1.BackColor = Color.FromArgb(40, 45, 50);
             button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(118, 178, 187);
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Ubuntu", 20F, FontStyle.Regular, GraphicsUnit.Point);
             button1.ForeColor = Color.FromArgb(235, 235, 250);
-            button1.Location = new Point(3, 490);
+            button1.Location = new Point(3, 653);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(250, 70);
+            button1.Size = new Size(286, 93);
             button1.TabIndex = 0;
             button1.Text = "New Material";
             button1.UseVisualStyleBackColor = false;
@@ -63,12 +65,14 @@
             button2.AutoSize = true;
             button2.BackColor = Color.FromArgb(40, 45, 50);
             button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(118, 178, 187);
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Ubuntu", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
             button2.ForeColor = Color.FromArgb(235, 235, 250);
-            button2.Location = new Point(601, 490);
+            button2.Location = new Point(687, 653);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(250, 70);
+            button2.Size = new Size(286, 93);
             button2.TabIndex = 1;
             button2.Text = "Open Material";
             button2.UseVisualStyleBackColor = false;
@@ -81,9 +85,10 @@
             panel1.Controls.Add(button1);
             panel1.Controls.Add(button2);
             panel1.ForeColor = Color.Black;
-            panel1.Location = new Point(10, 41);
+            panel1.Location = new Point(11, 55);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(854, 563);
+            panel1.Size = new Size(976, 751);
             panel1.TabIndex = 2;
             // 
             // TitleBar
@@ -94,8 +99,9 @@
             TitleBar.Controls.Add(Close);
             TitleBar.Dock = DockStyle.Top;
             TitleBar.Location = new Point(0, 0);
+            TitleBar.Margin = new Padding(3, 4, 3, 4);
             TitleBar.Name = "TitleBar";
-            TitleBar.Size = new Size(876, 35);
+            TitleBar.Size = new Size(1001, 47);
             TitleBar.TabIndex = 3;
             TitleBar.MouseDown += MoveWindow;
             // 
@@ -103,12 +109,15 @@
             // 
             Min.Dock = DockStyle.Right;
             Min.FlatAppearance.BorderSize = 0;
+            Min.FlatAppearance.MouseDownBackColor = Color.FromArgb(118, 178, 187);
+            Min.FlatAppearance.MouseOverBackColor = Color.FromArgb(118, 178, 187);
             Min.FlatStyle = FlatStyle.Flat;
             Min.ForeColor = Color.FromArgb(64, 64, 64);
             Min.Image = (Image)resources.GetObject("Min.Image");
-            Min.Location = new Point(696, 0);
+            Min.Location = new Point(794, 0);
+            Min.Margin = new Padding(3, 4, 3, 4);
             Min.Name = "Min";
-            Min.Size = new Size(60, 35);
+            Min.Size = new Size(69, 47);
             Min.TabIndex = 2;
             Min.UseVisualStyleBackColor = false;
             Min.Click += MinWindow;
@@ -117,12 +126,15 @@
             // 
             Max.Dock = DockStyle.Right;
             Max.FlatAppearance.BorderSize = 0;
+            Max.FlatAppearance.MouseDownBackColor = Color.FromArgb(215, 215, 230);
+            Max.FlatAppearance.MouseOverBackColor = Color.FromArgb(235, 235, 250);
             Max.FlatStyle = FlatStyle.Flat;
             Max.ForeColor = Color.FromArgb(64, 64, 64);
             Max.Image = (Image)resources.GetObject("Max.Image");
-            Max.Location = new Point(756, 0);
+            Max.Location = new Point(863, 0);
+            Max.Margin = new Padding(3, 4, 3, 4);
             Max.Name = "Max";
-            Max.Size = new Size(60, 35);
+            Max.Size = new Size(69, 47);
             Max.TabIndex = 1;
             Max.UseVisualStyleBackColor = false;
             Max.Click += MaxWindow;
@@ -131,26 +143,30 @@
             // 
             Close.Dock = DockStyle.Right;
             Close.FlatAppearance.BorderSize = 0;
+            Close.FlatAppearance.MouseDownBackColor = Color.DarkRed;
+            Close.FlatAppearance.MouseOverBackColor = Color.Red;
             Close.FlatStyle = FlatStyle.Flat;
             Close.ForeColor = Color.FromArgb(64, 64, 64);
             Close.Image = (Image)resources.GetObject("Close.Image");
-            Close.Location = new Point(816, 0);
+            Close.Location = new Point(932, 0);
+            Close.Margin = new Padding(3, 4, 3, 4);
             Close.Name = "Close";
-            Close.Size = new Size(60, 35);
+            Close.Size = new Size(69, 47);
             Close.TabIndex = 0;
             Close.UseVisualStyleBackColor = false;
             Close.Click += CloseWindow;
             // 
             // LandingPage
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(40, 40, 40);
-            ClientSize = new Size(876, 616);
+            ClientSize = new Size(1001, 821);
             Controls.Add(TitleBar);
             Controls.Add(panel1);
             ForeColor = Color.Transparent;
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "LandingPage";
             Text = "IGMaterialTool";
             panel1.ResumeLayout(false);
